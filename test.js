1ceda476eca665bcb1f82d35b13e823580b78676
riafeed <riafeed@mail.goo.ne.jp>
2011-12-07 16:04:59 +0000

    x

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-var express = require('express');
- 
-var app = express.createServer();
- 
-app.get('/', function(req, res) {
-    res.send('Hello World');
-});
- 
-var port = 3000;
-app.listen(port);
+var http = require('http');
+var port = process.env.PORT || 3000;
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port);
