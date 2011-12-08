1c503f96060b9305b7468879d9e532871210e07c
riafeed <riafeed@mail.goo.ne.jp>
2011-12-08 13:55:19 +0000

    x

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ var port = process.env.PORT || 3000;
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World2\n');
 }).listen(port);
